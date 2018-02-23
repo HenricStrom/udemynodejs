465dfa8286199992d8f8d73b3c022821bf1a031f
Henric Ström <henric.strom@live.se>
2018-02-23 09:48:56 +0100

    stuff

--- app.js
+++ app.js
@@ -7,11 +7,18 @@ const yargs = require('yargs');
 const notes = require('./notes.js');
 
 const argv = yargs.argv;
+console.log(argv);
 var command = argv._[0];
 console.log('Command: ', command);
 
 if (command === 'add'){
-  notes.addNote(argv.title, argv.body);
+  var note = notes.addNote(argv.title, argv.body);
+  if (note){
+    console.log(`Note added! Title: ${note.body}, Body: ${note.body}`);
+  }
+  else{
+    console.log('The provided note already exists in the list.');
+  }
 }
 else if (command === 'list'){
   notes.getAll();
